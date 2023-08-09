e2d4eec36743f79073cfd1021549a83bab38aac8
Guru Mahan <iosdeve2022@gmail.com>
2023-08-09 16:56:45 +0530

    Initial Commit

---
+++ CreateNewPodFrameWork/CreateNewPodFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  CreateNewPodFrameWork.h
+//  CreateNewPodFrameWork
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CreateNewPodFrameWork.
+FOUNDATION_EXPORT double CreateNewPodFrameWorkVersionNumber;
+
+//! Project version string for CreateNewPodFrameWork.
+FOUNDATION_EXPORT const unsigned char CreateNewPodFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreateNewPodFrameWork/PublicHeader.h>
+
+
---
+++ CreateNewPodFrameWork/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  CreateNewPodFrameWork
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+import Foundation
+
+public class validator {
+    public var cammand: String = "Guru Mahan"
+}
